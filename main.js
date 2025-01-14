81861b9c8ba67b5b5164f2a7848fef2667d58a67
chiaralle <chiaraallegrino@gmail.com>
2025-01-14 12:46:05 +0100

    add: ciclo for per email

--- main.js
+++ main.js
@@ -9,3 +9,31 @@ Si può fare? Certo che si basta ragionare un po’.
 Nota:
 Non è necessario provvedere alla validazione delle email
 */ 
+
+/*Mini task:
+-creo una variabile array che contenga una lista di n email 
+-creo un prompt per chiedere all'utente la sua email 
+-creo un ciclo per verificare se la mail è presente nella variabile (non posso utilizzare includes)
+-condizione se è presente (true) o non è presente (false) 
+
+*/ 
+const emailInvitati = [
+    'kirara@mail.com',
+    'totodile@mail.com',
+    'bulbasaur@mail.com',
+    'squirtle@mail.com',
+    'chansey@mail.com',
+    'celebi@mail.com',
+    'jynx@mail.com'
+  ];
+
+  let email = prompt ("Digita la tua email");
+
+  for (let i = 0; i < emailInvitati.length; i++) {
+    if ( ) {
+     console.log(``);
+   } else {
+     console.log(``);
+   }
+   
+ }
